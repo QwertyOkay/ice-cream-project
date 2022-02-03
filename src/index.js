fec8b03c5be06b7b0d0bbdb03a3e4108a4831e79
QwertyOkay <kpshmt@gmail.com>
2022-02-03 23:48:51 +0200

    2022

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './sass/main.scss';
+
+// import './js/modal-about.js';
+import './js/slider.js';
+import './js/menu.js';
+import './js/arrow.js';
+import './js/buynow.js';
+import './js/franchise';
